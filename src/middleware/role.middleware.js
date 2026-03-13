cf7070af0c1fa465b0158423c9ddc1277287e7a0
Yhaya Esmail <yhayaesmail@gmail.com>
2026-03-13 04:35:06 +0200

    fix frontend block and fix some classes confliction

--- src/middleware/role.middleware.js
+++ src/middleware/role.middleware.js
@@ -6,11 +6,11 @@ export const authorizeRole = (...roles) => {
     const user = req.user;
     if (!user) {
       logger.warn("Unauthorized access attempt without user");
-      return next(new ApiError("Unauthorized", 401));
+      return next(ApiError("Unauthorized", 401));
     }
     if (!roles.includes(user.role)) {
       logger.warn(`User ${user.id} tried to access restricted route`);
-      return next(new ApiError("Forbidden", 403));
+      return next(ApiError("Forbidden", 403));
     }
     next();
   };
