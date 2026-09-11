129e1dcb086ddbf4f840393898e0482b499b90ea
yhayaesmail <yhayaesmail@gmail.com>
2026-09-11 07:25:59 +0300

    linux conf

--- test.js
+++ test.js
@@ -2,8 +2,12 @@ import "dotenv/config";
 import { login } from "./src/modules/auth/auth.service.js";
 
 await login({
-  email: "test@test.com",
-  password: "123456",
+  email: "admin@hrm.dev",
+  password: "admin123",
 });
 
-console.log("Test finished");
+
+console.log(` ------------------------------`);
+console.log(` --------Test finished---------`);
+console.log(` ------------------------------`);
+
